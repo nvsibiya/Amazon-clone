f7da2f7cdbbab926d37fa9da24c87fb730d73de0
Nomfundo Verah Sibiya <nvsibiya0@gmail.com>
2020-10-27 22:40:57 +0100

    firebase config

---
+++ amazon-clone/src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBlAwhHyw2kPdZ4dU7XZj8Kkuo6heCAYxU",
+    authDomain: "clone-e4d8a.firebaseapp.com",
+    databaseURL: "https://clone-e4d8a.firebaseio.com",
+    projectId: "clone-e4d8a",
+    storageBucket: "clone-e4d8a.appspot.com",
+    messagingSenderId: "606818337637",
+    appId: "1:606818337637:web:43e5dd319ef25c6b51978c",
+    measurementId: "G-CWGRBG65E9"
+  };
